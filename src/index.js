0ce4807210a6961b17afbf07b7cdc1e27f6db344
Sang Choi <akasyashc@gmail.com>
2017-01-02 08:57:34 -0800

    Add protected content with HOC

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import reduxThunk from 'redux-thunk'
 import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
-import Signup from './components/auth/signup'
+import Signup from './components/auth/signup';
+import Feature from './components/feature';
+import RequireAuth from './components/auth/require_auth';
 import reducers from './reducers';
 
 import '../style/style.scss';
@@ -22,6 +24,7 @@ ReactDOM.render(
         	<Route path='signin' component={Signin} />
             <Route path='signout' component={Signout} />
             <Route path='signup' component={Signup} />
+            <Route path='feature' component={RequireAuth(Feature)} />
         </Route>
     </Router>
   </Provider>
